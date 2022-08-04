7a92d32f72b98048fb3b933b36155ce47e6dd865
Alfahan <ali.farhan160@gmail.com>
2022-08-04 11:25:59 +0700

    Module Object

---
+++ scripts/module-object.js
@@ -0,0 +1,14 @@
+export const firstName = "Mohamad";
+export const middleName = "Ali";
+export const lastName = "Farhan";
+
+export function hello(){
+    console.info("Hello from Ali");
+}
+
+export class Person {
+
+    constructor() {
+        this.name = "Ali";
+    }
+}
